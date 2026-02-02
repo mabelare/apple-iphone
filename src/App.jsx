fb6b12d17f5e540b02ac118d5699a6e0f328b338
mabel <mabelare293@gmail.com>
2026-02-02 20:17:40 +0100

    features, footer, howitworks

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import * as Sentry from "@sentry/react";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar /> <Hero /> <Highlights /> <Model />
+      <Features /> <HowItWorks /> <Footer />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://af84073c25b0a8f48a20a82014da5fb9@o4510817414873088.ingest.us.sentry.io/4510817418477568",
+
+  sendDefaultPii: true,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
